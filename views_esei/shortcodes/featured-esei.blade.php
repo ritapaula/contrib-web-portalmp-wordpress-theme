610c796d435484f4d1147d79ae1203a13ab66d02
Fernando Gonzalez <fernando.gonzalez@ideit.es>
2019-07-17 00:23:08 +0200

    Insertar atributo JSON para que no falle la extracción de traducciones.

--- views_esei/shortcodes/featured-esei.blade.php
+++ views_esei/shortcodes/featured-esei.blade.php
@@ -1,40 +1,45 @@
+@php
+$slider_options = [
+    'slidesToShow' => 4,
+    'slidesToScroll' => 1,
+    'autoplay' => false,
+    'responsive' => [
+        [
+            'breakpoint' => 1024,
+            'settings' => [
+                'slidesToShow' => 3,
+                'dots' => false,
+                'arrows' => true
+            ]
+        ],
+        [
+            'breakpoint' => 800,
+            'settings' => [
+                'slidesToShow' => 2,
+                'dots' => true,
+                'arrows' => false
+            ]
+        ],
+        [
+            'breakpoint' => 600,
+            'settings' => [
+                'slidesToShow' => 1,
+                'dots' => true,
+                'arrows' => false
+            ]
+        ]
+    ]
+];
+@endphp
 <section class="uvigo-featured-esei {{$featured_root_classname}}">
   <div class="featured pt-4 pb-6">
     <div class="container">
       <h2 class="featured__title h2 mt-12 mb-11">{{$featured_title}}</h2>
       <div class="slick-slider sliderbehaviour slick-slider-esei"
-        data-slick='{"slidesToShow": 4, "slidesToScroll": 1, "autoplay": false,
-          "responsive": [
-            {
-              "breakpoint": 1024,
-              "settings": {
-                "slidesToShow": 3,
-                "dots": false,
-                "arrows": true
-              }
-            },
-            {
-              "breakpoint": 800,
-              "settings": {
-                "slidesToShow": 2,
-                "dots": true,
-                "arrows": false
-              }
-            },
-            {
-              "breakpoint": 600,
-              "settings": {
-                "slidesToShow": 1,
-                "dots": true,
-                "arrows": false
-              }
-            }
-          ]}'>
-
+        data-slick='@json($slider_options)'>
             @foreach ($featured_list as $item)
-              {!! $item['html'] !!}
+                {!! $item['html'] !!}
             @endforeach
-
       </div>
       @if ($featured_button_title !== '')
       <div class="featured__footer row justify-content-end">

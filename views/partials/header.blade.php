f7485d05a377a902ec7e9a222986827747d11855
Fernando Gonzalez <fernando.gonzalez@ideit.es>
2019-07-17 00:22:32 +0200

    Ajustes de estilos y traducciones.

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -3,7 +3,7 @@
   <div id="currentsearch" class="currentsearch">
     <div class="currentsearch__container">
         <div class="currentsearch__content align-items-center">
-        <button type="button" id="globalsearch__toggle-button" data-toggle="search" data-target="#currentsearch" class="btn globalsearch__toggle-button close-button" data-icon="&#x4d;"><span class="sr-only">{{ _e( 'Close', 'uvigothemewp' ) }}</span></button>
+        <button type="button" id="globalsearch__toggle-button" data-toggle="search" data-target="#currentsearch" class="btn globalsearch__toggle-button close-button" data-icon="&#x4d;"><span class="sr-only">{{ __( 'Close', 'uvigothemewp' ) }}</span></button>
         <div class="col-6 offset-3">
             {{ get_search_form() }}
         </div>
--- views/partials/searchform.blade.php
+++ views/partials/searchform.blade.php
@@ -1,12 +1,12 @@
 <div id="widgetsearch" class="widgetsearch">
   <form role="search" method="get" class="globalsearch__form" action="{{ esc_url( home_url( '/' )) }}">
     <label class="globalsearch__form__label">
-      <span class="screen-reader-text">{{ _e('Search for:', 'uvigothemewp') }}</span>
-      <input type="search" class="form-control" placeholder="{{ _e('Insert words to search', 'uvigothemewp') }}&hellip;" value="{{ get_search_query() }}" name="s" />
+      <span class="screen-reader-text">{{ __('Insert words to search', 'uvigothemewp') }}</span>
+      <input type="search" class="form-control" placeholder="{{ __('Insert words to search', 'uvigothemewp') }}&hellip;" value="{{ get_search_query() }}" name="s" />
     </label>
     <div class="globalsearch__form__button">
-      <button type="submit" class="btn btn-outline-light btn-icon" data-icon="&#x35;">{{ _e( 'Search', 'uvigothemewp' ) }}</button>
+      <button type="submit" class="btn btn-outline-light btn-icon" data-icon="&#x35;">{{ __( 'Search', 'uvigothemewp' ) }}</button>
     </div>
   </form>
 </div>
-{{-- <button type="button" id="globalsearch__toggle-button" data-toggle="search" data-target="#currentsearch" class="btn globalsearch__toggle-button" data-icon="&#x55;"><span class="sr-only">{{ _e( 'Search', 'uvigothemewp' ) }}</span></button> --}}
+{{-- <button type="button" id="globalsearch__toggle-button" data-toggle="search" data-target="#currentsearch" class="btn globalsearch__toggle-button" data-icon="&#x55;"><span class="sr-only">{{ __( 'Search', 'uvigothemewp' ) }}</span></button> --}}
